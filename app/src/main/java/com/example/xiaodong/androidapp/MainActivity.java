f2d064e9cc88c4bb69656ae78d90b4f3d69b1fc4
sixiaodong <sixiaodong168@gmail.com>
2015-09-21 11:17:35 +0900

    editview 学习

--- app/src/main/java/com/example/xiaodong/androidapp/MainActivity.java
+++ app/src/main/java/com/example/xiaodong/androidapp/MainActivity.java
@@ -1,11 +1,29 @@
 package com.example.xiaodong.androidapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.xiaodong.androidapp.MESSAGE";
+    //xnm的按钮事件调本方法
+    public void sendMessage(View view){
+        //从当前窗口跳转到DisplayMessageActivity
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        //获取文本框的id
+        EditText editText = (EditText)findViewById(R.id.edit_message);
+        //获取文本框内容
+        String message = editText.getText().toString();
+        //向另一个窗口传值
+        intent.putExtra(EXTRA_MESSAGE,message);
+        //启动跳转
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
